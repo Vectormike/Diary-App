34e7b4118f5be94aa68ccc57dd030438c258f8bf
Vectormike40 <victor.junior40@yahoo.com>
2018-11-30 13:00:45 +0100

    Routes configured

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const expressSanitizer = require('express-sanitizer');
 const router = express.Router();
 const index = require('./routes/index');
 const add = require('./routes/add');
-
+const show = require('./routes/show');
+const edit =  require('./routes/edit');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,53 +18,17 @@ app.use(methodOverride("_method"));
 
 //Connect all routes to the application
 app.use('/', index);
-app.use('/add', add);
+app.use('/', add);
+app.use('/', show);
+app.use('/', edit);
 
 
 
 
 
-
-
-//CREATE route
-app.post("/index", (req, res) => {
-    //Create new post in Mongoose
-    req.body.sanitized = req.sanitize(req.body.saySomething);
-    blog.create(req.body.sanitized, function(err, newPost){
-        if(err){
-            //Refresh page
-            res.render("new");
-        } else {
-            //Redirect to the index page
-            res.redirect("/index");
-        }
-    });
-});
-
-//SHOW route
-app.get("/index/:id", (req, res) => {
-    blog.findById(req.params.id, (err, foundPost) => {
-        if(err){
-           res.send("Error page"); 
-        } else {
-            res.render("info", {post:foundPost})
-        }
-    });
-});
-
-//EDIT route 
-app.get("/index/:id/edit", (req, res) => {
-    blog.findById(req.params.id, (err, editPost) => {
-        if(err){
-            res.redirect('/index');
-        } else {
-            res.render('edit', {blog:editPost});
-        }
-    });
-});
 
 //UPDATE route
-app.put('/index/:id', (req, res) => {
+router.put('/index/:id', (req, res) => {
     req.body.sanitized = req.sanitize(req.body.saySomething);
     blog.findOneAndUpdate(req.params.id, req.body.blog, (err, updatedPost) => {
         if(err){
@@ -75,7 +40,7 @@ app.put('/index/:id', (req, res) => {
 });
 
 //DESTROY route
-app.get('/index/:id/delete', (req, res) => {
+router.get('/index/:id/delete', (req, res) => {
     blog.findOneAndDelete(req.params.id, (err, deleteD) => {
         if(err){
             res.redirect('/index/:id');
