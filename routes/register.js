605a0e3346a6c9d559bec02b6bd5a357f5ab7f35
Vectormike40 <victor.junior40@yahoo.com>
2018-12-10 15:59:00 +0100

    Registration auth modified but not creating user

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose', {useNewUrlParser: true});
 
 
 
@@ -11,18 +11,29 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-    req.body.username
-    req.body.email
-    req.body.password
-    User.register(new User({username:req.body.username, username2:req.body.email}), req.body.password, (err, user) => {
-        if(err){
-            //Error message
-            console.log(err);
-            return res.send(err);
-        } else {
-            res.send('Passed');
-        }
-    })
+    let password = req.body.password;
+    let password2 = req.body.password2
+
+    // User not creating, passing else statement instead
+    if(password == password2){
+        var newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+
+        });
+        
+        User.createUser(newUser, (err, user) =>{
+            if(err){
+                console.log(err);
+            } else{
+                res.send('Passed');
+            }
+        })
+    } else{
+        res.send('Password mismatch');
+    }
+
 });
 
 module.exports = router;
