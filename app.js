b2481ca9bb70263c49869bc7d37ad4af5336a050
Vectormike40 <victor.junior40@yahoo.com>
2018-12-11 09:51:50 +0100

    Register auth modified but not saving to db

--- app.js
+++ app.js
@@ -1,22 +1,78 @@
 const express = require("express");
 const app = express();
+const bcrypt = require('bcryptjs');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const methodOverride = require('method-override');
 const expressSanitizer = require('express-sanitizer');
 const router = express.Router();
+
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+
+// Express Session
+app.use(require('express-session')({
+  secret: "Vectormike",
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
 const home = require('./routes/home');
 const index = require('./routes/index');
 const add = require('./routes/add');
 const show = require('./routes/show');
 const edit =  require('./routes/edit');
 const remove = require('./routes/delete');
+const register = require('./routes/register');
+const login = require('./routes/login');
+const User = require('./models/user');
+
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+
+
+
+passport.use(new localStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+// used to serialize the user for the session
+passport.serializeUser(function(user, done) {
+    done(null, user.id); 
+   // where is this user.id going? Are we supposed to access this anywhere?
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
 //Connect all routes to the application
 app.use('/', home);
@@ -25,8 +81,10 @@ app.use('/', add);
 app.use('/', show);
 app.use('/', edit);
 app.use('/', remove);
+app.use('/', register);
+app.use('/', login);
 
 // Listening on this port
 app.listen(3000, function(){
-    console.log("Blog is running");
+    console.log("Diary is running");
 });
