ab0a1a64b4bfe55eea8a6739a807dc5e78d953d3
Vectormike40 <victor.junior40@yahoo.com>
2018-12-19 12:33:54 +0100

    Login config

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
+// --> Login page
 router.get('/login', (req, res) => {
     res.render('login');
+});
+
+// --> Login config
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/secret',
+    failureRedirect: '/login'
+}), (req, res) => {
+
 });
 
 module.exports = router;
